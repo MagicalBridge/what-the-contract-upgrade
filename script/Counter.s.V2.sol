cdb8bd66a489f202145eab7ef3f9497d0b592635
chupengfei <450793585@qq.com>
2024-07-30 11:21:11 +0800

    feat: upgrade success

--- script/Counter.s.V2.sol
+++ script/Counter.s.V2.sol
@@ -2,8 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {Upgrades, Options} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 import {CounterV2} from "../src/CounterV2.sol";
 
 contract CounterScriptV2 is Script {
@@ -11,8 +10,20 @@ contract CounterScriptV2 is Script {
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address proxyAddress = vm.envAddress("PROXY_ADDRESS");
+        address deployerAddress = vm.addr(deployerPrivateKey);
+
+        console.log("Deployer address:", deployerAddress);
 
         vm.startBroadcast(deployerPrivateKey);
+
+        Options memory opts;
+
+        opts.unsafeSkipAllChecks = true;
+        opts.referenceContract = "Counter.sol";
+
+        // 这里我使用UUPS的模式进行升级
+        Upgrades.upgradeProxy(proxyAddress, "CounterV2.sol", "", opts, deployerAddress);
 
         vm.stopBroadcast();
     }
