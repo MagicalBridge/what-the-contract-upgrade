59bedb12203e98239a4d8f79bc725a0221ef4b07
chupengfei <450793585@qq.com>
2024-07-30 09:01:11 +0800

    feat: turn contracts into scalable models

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+contract Counter is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
@@ -11,4 +14,12 @@ contract Counter {
     function increment() public {
         number++;
     }
+
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+        number = 0;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 }
